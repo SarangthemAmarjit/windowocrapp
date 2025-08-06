a130aaba9799f66f68cea8de4c007572bc636bce
Sarangthem Amarjit Meetei <sarangthemamarjit123@gmail.com>
2025-08-06 12:16:24 +0530

    updated

--- lib/cons/apis.dart
+++ lib/cons/apis.dart
@@ -1,8 +1,8 @@
 // String permitapi = "https://manipurilponline.mn.gov.in/api/permit/";
 String permitapi = "${api}api/permit/";
-// String api = "https://manipurilponline.mn.gov.in/";
-String api = "http://10.10.1.96/";
+String api = "https://manipurilponline.mn.gov.in/";
+// String api = "http://10.10.1.96/";
 // String api2 = "https://manipurilponline.mn.gov.in/";
-// String localapi = "https://manipurilponline.mn.gov.in";
+String localapi = "https://manipurilponline.mn.gov.in";
 // String localapi = "https://ilpdemo.cubeten.com/";
-String localapi = "http://10.10.1.96/";
+// String localapi = "http://10.10.1.96/";
--- lib/cons/constant.dart
+++ lib/cons/constant.dart
@@ -2,7 +2,7 @@ List<String> genders = ["Male", "Female", "Others"];
 String gate = "Imphal Airport";
 String rupee = "₹";
 bool onlinePayment = true;
-bool isDebugmode = true;
+bool isDebugmode = false;
 String printername = "CUSTOM K80";
 final List<String> cardTypes = ['Aadhar', 'PAN', 'Voter', 'Driving Licence'];
 final List<String> purposes = ["Tourist", "Official", "Business", "Others"];
@@ -46,7 +46,12 @@ List<String> states = [
   "West Bengal"
 ];
 
-final List<String> documentTypes = ['Driving License', 'Passport', 'Aadhaar Card', 'Pan Card'];
+final List<String> documentTypes = [
+  'Driving License',
+  'Passport',
+  'Aadhaar Card',
+  'Pan Card'
+];
 
 List<String> termcondition = [
   "a)I shall be responsible for the good conduct during my stay in the state of Manipur.",
